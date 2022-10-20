9d3c90cb1b9f56cd58488f8a73c370df9887dd8b
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-10-20 22:24:07 +0530

    push

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const items = require("./data/team");
 const info = require("./data/info");
 const player = require("./data/player");
 const login = require("./data/login");
+const job = require("./data/jobs");
 
 
 
 module.exports = {
+  job,
   items,
   info,
   player,
